7e792e5473d8c6e5d31e2c0b1eedebf302d4d182
maheendran kp <maheendrankp100@gmail.com>
2023-05-25 11:56:46 +0530

    updated review section

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ connectDB();
 app.listen(3000, () => {
   console.log('port is connected');
 });
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
